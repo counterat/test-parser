03f245876941f79eee352ad504238a261e9bf363
counterat <counteratios@gmail.com>
2025-03-10 21:56:06 +0200

    parse captions+likes+comments

--- parse/parser.py
+++ parse/parser.py
@@ -2,20 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-from utils import convert_strings_into_dicts
+from utils import convert_strings_into_dicts, get_post_with_link, scroll_until_element_is_fully_visible
 from config import cookies_string
 from selenium.webdriver.common.action_chains import ActionChains
 import asyncio
 
 
-posts = [
-    
-]
-
 class Parser:
     def __init__(self, url, cookie_string):
         self._default_time_of_sleep = 7
         
+        self._posts = []
         self.url = url
         self.cookie_string = cookie_string     
         
@@ -30,14 +27,63 @@ class Parser:
         for name, value in cookies_dict.items():
             self.driver.add_cookie({"name":name, "value":value})
 
+    @property
+    def posts(self):
+        return self._posts
+    
+    @posts.setter
+    def posts(self, new):
+        self._posts = new
+    
+    async def _get_link(self, post):
+        parent_elem = post.find_element(By.XPATH, "..")
+        post_link = parent_elem.get_attribute("href")
+        return post_link, parent_elem
+
+    async def _get_likes_and_comments_amount(self, post, parent_elem):
+        self.actions.move_to_element(post).perform()
+        await asyncio.sleep(1)
+        stats = parent_elem.find_elements(By.CLASS_NAME, "x1ey2m1c")[-1]
+        likes_and_comments = stats.find_elements(By.TAG_NAME, "span")
+        likes = likes_and_comments[0].text
+        comments = likes_and_comments[3].text
+        return likes, comments
+    
+    async def _get_caption(self, post):
+        
+        scroll_until_element_is_fully_visible(self.actions, post)
+
+        await asyncio.sleep(3)
+        
+        all_h1s = self.driver.find_elements(By.TAG_NAME, "h1")[1:]
+        for h1 in all_h1s:
+            print(h1.text)
+        close_btn = self.driver.find_element(By.CSS_SELECTOR, "body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x160vmok.x10l6tqk.x1eu8d0j.x1vjfegm > div > div")
+        scroll_until_element_is_fully_visible(self.actions, close_btn)
+
+        await asyncio.sleep(2)
+
     async def _get_all_available_posts(self):
         print("searching for posts")
         all_available_posts = self.driver.find_elements(By.CSS_SELECTOR, "._aagu")
+        duplicate_element_found = False
         for post in all_available_posts:
-            parent_elem = post.find_element(By.XPATH, "..")
-            print(parent_elem, parent_elem.get_attribute("href"))
+            if True:
+                post_link, parent_elem = await self._get_link(post)
+                print(f"POST FOUND, LINK - {post_link}")
+                post_in_posts = get_post_with_link(self.posts, post_link)
+                if post_in_posts:
+                    duplicate_element_found = True
+                    break
+                
+                likes, comments = await self._get_likes_and_comments_amount(post, parent_elem)
+                
+                caption = await self._get_caption(post)
+                
+                self._posts.append({"link":post_link, "likes":likes, "comments":comments})
             
-    
+        return duplicate_element_found
+        
     async def sleep_some_time(self):
         await asyncio.sleep(self._default_time_of_sleep)
     
--- utils.py
+++ utils.py
@@ -2,3 +2,11 @@ def convert_strings_into_dicts(cookie_string):
     cookies_dict = dict(item.split("=", 1) for item in cookie_string.split("; "))
     return cookies_dict
 
+def get_post_with_link(posts:list, link:str):
+    for post in posts:
+        if post.get("link") == link:
+            return post
+    return False
+
+def scroll_until_element_is_fully_visible(actions, element):
+    actions.move_to_element(element).click().perform()
